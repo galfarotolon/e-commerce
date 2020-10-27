d5e1c0e6d636fcc13216f688a31d53fd086b3aad
Guillermo Alfaro <galfarotolon@gmail.com>
2020-10-27 13:49:32 -0500

    added backend server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,13 +8,11 @@ function HomeScreen() {
         <>
             <h1>Latest Products</h1>
             <Row>
-                {
-                    products.map(product => (
-                        <Col sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))
-                }
+                {products.map(product => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                    </Col>
+                ))}
             </Row>
         </>
     )
