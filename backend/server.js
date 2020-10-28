897a5633f7c167d31f97fdeb5a7de3d190475f70
Guillermo Alfaro <galfarotolon@gmail.com>
2020-10-28 11:31:13 -0500

    added db config with mongoose, and models for orders, products, users

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 
 dotenv.config();
+
+connectDB()
 
 const app = express()
 
@@ -22,4 +26,4 @@ app.get('/api/products/:id', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} on ${PORT}`))
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} on ${PORT}`.yellow.bold))
