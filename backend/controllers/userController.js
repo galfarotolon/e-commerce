74effae279eee434ab16f93f7286809d6ebe01b2
Guillermo Alfaro <galfarotolon@gmail.com>
2020-10-29 11:49:56 -0500

    password encrypt and user registration complete in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,6 +30,41 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc Register new user
+// @route POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    //destructure from body
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User Already Exists')
+    }
+
+    const user = await User.create({
+        name, email, password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+
+        })
+    } else {
+        res.status(400)
+        throw new Error('invalid user data')
+
+    }
+
+})
 
 // @desc Get user profile
 // @route GET /api/users/profile
@@ -56,4 +91,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,16 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+    //if password hasn't been modified: call next
+    if (!this.isModified('password')) {
+        next()
+    }
+    //if it has then hash password
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
