6da75ae6876b326603781f222110b6a39f6672a0
Guillermo Alfaro <galfarotolon@gmail.com>
2020-11-02 20:12:26 -0500

    added delete user for admin only

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -145,4 +145,27 @@ const getUsers = asyncHandler(async (req, res) => {
 })
 
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
+// @desc delete user
+// @route DELETE /api/users/:id
+// @access Private/Admin
+
+const deleteUser = asyncHandler(async (req, res) => {
+
+
+    // const user = await User.findById(req.user._id)
+
+    const user = await User.findById(req.params.id)
+
+    if (user) {
+        await user.remove()
+        res.json({ message: 'User removed' })
+    } else {
+        res.status(404)
+        throw new Error('User Not Found')
+    }
+
+
+})
+
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
