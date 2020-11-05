64ac6166768a739f3144757a1b742f7b4d8b6f4c
Guillermo Alfaro <galfarotolon@gmail.com>
2020-11-05 13:03:37 -0500

    added pagination to products and admin products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,6 +10,10 @@ import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler(async (req, res) => {
 
+    const pageSize = 4
+    const page = Number(req.query.pageNumber) || 1
+
+
     //regex to include any letter (not just the full word ) to search for items
     //options 'i' is to make the search case insensitive
 
@@ -20,9 +24,11 @@ const getProducts = asyncHandler(async (req, res) => {
         }
     } : {}
 
-    const products = await Product.find({ ...keyword })
+    const count = await Product.countDocuments({ ...keyword })
 
-    res.json(products)
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1))
+
+    res.json({ products, page, pages: Math.ceil(count / pageSize) })
 
 })
 
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,14 +4,19 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
+
+    const pageNumber = match.params.pageNumber || 1
+
+
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, pages, page } = productList
 
 
     const productDelete = useSelector(state => state.productDelete)
@@ -35,11 +40,12 @@ const ProductListScreen = ({ history, match }) => {
         if (successCreate) {
             history.push(`/admin/product/${createdProduct._id}/edit`)
         } else {
-            dispatch(listProducts())
+            //keyword is an empty string since listproducts takes 2 params
+            dispatch(listProducts('', pageNumber))
         }
 
 
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct, pageNumber])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure you want to delete user?')) {
@@ -75,49 +81,53 @@ const ProductListScreen = ({ history, match }) => {
             {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
 
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-                <Table striped bordered hover responsive className='table-sm'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>NAME</th>
-                            <th>PRICE</th>
-                            <th>CATEGORY</th>
-                            <th>BRAND</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map(product => (
-                            <tr key={product._id}>
-                                <td>{product._id}</td>
-                                <td>{product.name}</td>
-                                <td>${product.price}</td>
-                                <td>{product.category}
-                                </td>
-                                <td>{product.brand}</td>
-                                <td>
-                                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                                        <Button variant='light' className='btn-sm'>
-                                            <i className='fas fa-edit'></i>
-                                        </Button>
-                                    </LinkContainer>
-                                    <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
-
-                                        <i className='fas fa-trash'></i>
-                                    </Button>
-
-
-
-                                </td>
-
+                <>
+                    <Table striped bordered hover responsive className='table-sm'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>PRICE</th>
+                                <th>CATEGORY</th>
+                                <th>BRAND</th>
 
                             </tr>
-                        ))}
-                    </tbody>
+                        </thead>
+                        <tbody>
+                            {products.map(product => (
+                                <tr key={product._id}>
+                                    <td>{product._id}</td>
+                                    <td>{product.name}</td>
+                                    <td>${product.price}</td>
+                                    <td>{product.category}
+                                    </td>
+                                    <td>{product.brand}</td>
+                                    <td>
+                                        <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                            <Button variant='light' className='btn-sm'>
+                                                <i className='fas fa-edit'></i>
+                                            </Button>
+                                        </LinkContainer>
+                                        <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
+
+                                            <i className='fas fa-trash'></i>
+                                        </Button>
 
 
 
-                </Table>
+                                    </td>
+
+
+                                </tr>
+                            ))}
+                        </tbody>
+
+
+
+                    </Table>
+                    <Paginate pages={pages} page={page} isAdmin={true} />
+
+                </>
             )}
 
         </div>
